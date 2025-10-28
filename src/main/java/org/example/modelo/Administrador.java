08fbc0a74df2b8ba43d391002f21315bb20964c1
volcano56v <hayatolegendario@gmail.com>
2025-10-28 14:17:37 -0300

    más código base

--- src/main/java/org/example/modelo/Administrador.java
+++ src/main/java/org/example/modelo/Administrador.java
@@ -1,12 +1,24 @@
 package org.example.modelo;
 
+import java.util.ArrayList;
+
 public class Administrador {
     private final String surname;
     private final String password;
+    private ArrayList<Bodega> bodegas;
+    private ArrayList<Estante> estantes;
+    private ArrayList<Producto> productos;
 
-    Administrador(String surname, String password) {
+    public Administrador(String surname, String password) {
         this.surname = surname;
         this.password = password;
+        this.bodegas = new ArrayList<>();
+        this.estantes = new ArrayList<>();
+        this.productos = new ArrayList<>();
+    }
+
+    public String getSurname() {
+        return surname;
     }
 
     @Override
@@ -14,6 +26,9 @@ public class Administrador {
         return "Administrador{" +
                 "surname='" + surname + '\'' +
                 ", password='" + password + '\'' +
+                ", bodegas=" + bodegas +
+                ", estantes=" + estantes +
+                ", productos=" + productos +
                 '}';
     }
 }
--- src/main/java/org/example/modelo/Cliente.java
+++ src/main/java/org/example/modelo/Cliente.java
@@ -1,17 +1,25 @@
 package org.example.modelo;
 
+import java.util.ArrayList;
+
 public class Cliente {
     private String rut;
     private int dinero;
+    private ArrayList<Estante> estantes;
+    private ArrayList<Producto> productos;
 
     public Cliente(String rut, int dinero) {
         this.rut = rut;
         this.dinero = dinero;
+        this.estantes = new ArrayList<>();
+        this.productos = new ArrayList<>();
     }
 
     public Cliente(int dinero){
         this.dinero = dinero;
         this.rut = null;
+        this.estantes = new ArrayList<>();
+        this.productos = new ArrayList<>();
     }
 
     @Override
@@ -19,6 +27,8 @@ public class Cliente {
         return "Cliente{" +
                 "rut='" + rut + '\'' +
                 ", dinero=" + dinero +
+                ", estantes=" + estantes +
+                ", productos=" + productos +
                 '}';
     }
 }
